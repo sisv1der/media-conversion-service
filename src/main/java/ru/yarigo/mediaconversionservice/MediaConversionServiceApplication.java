f7f1088a52bd21d428eaf6b433b356644f732412
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-01-12 18:57:44 +0300

    init

---
+++ src/main/java/ru/yarigo/mediaconversionservice/MediaConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.yarigo.mediaconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaConversionServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MediaConversionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yarigo/mediaconversionservice/MediaConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yarigo.mediaconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
