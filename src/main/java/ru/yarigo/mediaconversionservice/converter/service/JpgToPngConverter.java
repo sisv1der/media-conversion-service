47530c9ffb9ec1dfc8300e85adcd89c70ae166b0
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-01-25 22:43:18 +0300

    feat: добавил JPEG конвертер

---
+++ src/main/java/ru/yarigo/mediaconversionservice/converter/service/JpgToPngConverter.java
@@ -0,0 +1,34 @@
+package ru.yarigo.mediaconversionservice.converter.service;
+
+import org.springframework.stereotype.Service;
+import ru.yarigo.mediaconversionservice.converter.ConversionKey;
+import ru.yarigo.mediaconversionservice.converter.Converter;
+import ru.yarigo.mediaconversionservice.converter.MediaFormat;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+
+@Service
+class JpgToPngConverter implements Converter {
+
+    @Override
+    public ConversionKey key() {
+        return new ConversionKey(MediaFormat.JPG, MediaFormat.PNG);
+    }
+
+    @Override
+    public void convert(Path inputPath, Path outputPath) throws IOException {
+        BufferedImage image = ImageIO.read(inputPath.toFile());
+        if (image == null) {
+            throw new IllegalStateException("Не удалось прочитать изображение");
+        }
+
+        boolean ok = ImageIO.write(image, "png", outputPath.toFile());
+        if (!ok) {
+            throw new IllegalStateException("ImageIO не смог записать PNG");
+        }
+    }
+}
