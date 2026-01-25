5497abce96c1ab06b3cf960de92ad0e4a9c0e7ec
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-01-25 22:21:32 +0300

    refactor: вынес стандартную валидацию через FFmpeg в default метод Validator. Очень сильно меньше кода

--- src/main/java/ru/yarigo/mediaconversionservice/validation/service/WebmValidator.java
+++ src/main/java/ru/yarigo/mediaconversionservice/validation/service/WebmValidator.java
@@ -1,12 +1,8 @@
 package ru.yarigo.mediaconversionservice.validation.service;
 
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FrameGrabber;
 import org.springframework.stereotype.Service;
 import ru.yarigo.mediaconversionservice.converter.MediaFormat;
 import ru.yarigo.mediaconversionservice.validation.Validator;
-
-import java.nio.file.Path;
 
 @Service
 class WebmValidator implements Validator {
@@ -14,19 +10,5 @@ class WebmValidator implements Validator {
     @Override
     public MediaFormat mediaFormat() {
         return MediaFormat.WEBM;
-    }
-
-    @Override
-    public boolean isValid(Path file) {
-        try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(file.toFile())) {
-            grabber.start();
-            boolean hasVideo = grabber.getLengthInFrames() > 0 || grabber.getVideoCodec() != 0;
-            boolean formatOk = grabber.getFormat() != null && grabber.getFormat().contains("webm");
-
-            grabber.stop();
-            return hasVideo && formatOk;
-        } catch (FrameGrabber.Exception e) {
-            return false;
-        }
     }
 }
