dee9994a3569b79c6d8293fe7991f317b03e3753
Ivan Yarigo <140123959+sisv1der@users.noreply.github.com>
2026-01-25 22:30:44 +0300

    feat: add mp3 to wav converter (#2)
    * refactor: вынес стандартную валидацию через FFmpeg в default метод Validator. Очень сильно меньше кода
    
    * feat: добавил WAV валидатор
    
    * feat: добавил WavToMp3Converter
    
    ---------
    
    Co-authored-by: Ivan Yarigo <yarigo2018@yarigo.ru>

--- src/main/java/ru/yarigo/mediaconversionservice/validation/service/Mp3Validator.java
+++ src/main/java/ru/yarigo/mediaconversionservice/validation/service/Mp3Validator.java
@@ -1,12 +1,8 @@
 package ru.yarigo.mediaconversionservice.validation.service;
 
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FrameGrabber;
 import org.springframework.stereotype.Service;
 import ru.yarigo.mediaconversionservice.converter.MediaFormat;
 import ru.yarigo.mediaconversionservice.validation.Validator;
-
-import java.nio.file.Path;
 
 @Service
 class Mp3Validator implements Validator {
@@ -14,18 +10,5 @@ class Mp3Validator implements Validator {
     @Override
     public MediaFormat mediaFormat() {
         return MediaFormat.MP3;
-    }
-
-    @Override
-    public boolean isValid(Path file) {
-        try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(file.toFile())) {
-            grabber.start();
-            boolean formatOk = grabber.getFormat() != null && grabber.getFormat().contains("mp3");
-
-            grabber.stop();
-            return formatOk;
-        } catch (FrameGrabber.Exception e) {
-            return false;
-        }
     }
 }
--- src/main/java/ru/yarigo/mediaconversionservice/validation/service/Mp4Validator.java
+++ src/main/java/ru/yarigo/mediaconversionservice/validation/service/Mp4Validator.java
@@ -1,12 +1,8 @@
 package ru.yarigo.mediaconversionservice.validation.service;
 
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FrameGrabber;
 import org.springframework.stereotype.Service;
 import ru.yarigo.mediaconversionservice.converter.MediaFormat;
 import ru.yarigo.mediaconversionservice.validation.Validator;
-
-import java.nio.file.Path;
 
 @Service
 class Mp4Validator implements Validator {
@@ -14,19 +10,5 @@ class Mp4Validator implements Validator {
     @Override
     public MediaFormat mediaFormat() {
         return MediaFormat.MP4;
-    }
-
-    @Override
-    public boolean isValid(Path file) {
-        try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(file.toFile())) {
-            grabber.start();
-            boolean hasVideo = grabber.getLengthInFrames() > 0 || grabber.getVideoCodec() != 0;
-            boolean formatOk = grabber.getFormat() != null && grabber.getFormat().contains("mp4");
-
-            grabber.stop();
-            return hasVideo && formatOk;
-        } catch (FrameGrabber.Exception e) {
-            return false;
-        }
     }
 }
