cfe559f2b526f68dfcc9322a3c013e74ebe77ea3
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-02-08 22:30:52 +0300

    refactor: внедрил FfmpegPipeline в аудиоконвертеры

--- src/main/java/ru/yarigo/mediaconversionservice/converter/service/WavToMp3Converter.java
+++ src/main/java/ru/yarigo/mediaconversionservice/converter/service/WavToMp3Converter.java
@@ -2,9 +2,7 @@ package ru.yarigo.mediaconversionservice.converter.service;
 
 import org.bytedeco.javacv.*;
 import org.springframework.stereotype.Service;
-import ru.yarigo.mediaconversionservice.converter.ConversionKey;
-import ru.yarigo.mediaconversionservice.converter.Convertible;
-import ru.yarigo.mediaconversionservice.converter.MediaFormat;
+import ru.yarigo.mediaconversionservice.converter.*;
 
 import java.nio.file.Path;
 
@@ -21,37 +19,16 @@ class WavToMp3Converter implements Convertible {
             Path inputPath,
             Path outputPath
     ) throws FFmpegFrameGrabber.Exception, FFmpegFrameRecorder.Exception {
-        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputPath.toFile());
-        FFmpegFrameRecorder recorder = null;
-        try {
-            grabber.start();
+        RecorderFactory recorderFactory = (_, g) -> new FFmpegFrameRecorder(
+                outputPath.toFile(),
+                g.getAudioChannels()
+        );
 
-            recorder = new FFmpegFrameRecorder(
-                    outputPath.toFile(),
-                    grabber.getAudioChannels()
-            );
-
-            recorder.setAudioChannels(grabber.getAudioChannels());
-            recorder.setSampleRate(grabber.getSampleRate());
-            recorder.setAudioCodecName("libmp3lame");
-            recorder.setFormat("mp3");
-            recorder.start();
-
-            Frame frame;
-            while ((frame = grabber.grab()) != null) {
-                if (frame.samples != null) {
-                    recorder.record(frame);
-                }
-            }
-        } catch (FrameGrabber.Exception | FrameRecorder.Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (recorder != null) {
-                recorder.stop();
-                recorder.release();
-            }
-            grabber.stop();
-            grabber.release();
-        }
+        new FfmpegPipeline(inputPath, outputPath, recorderFactory)
+                .step((g, r) -> r.setAudioChannels(g.getAudioChannels()))
+                .step((g, r) -> r.setSampleRate(g.getSampleRate()))
+                .step(r -> r.setAudioCodecName("libmp3lame"))
+                .step(r -> r.setFormat("mp3"))
+                .convert();
     }
 }
