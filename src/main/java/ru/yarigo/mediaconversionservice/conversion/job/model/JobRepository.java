18d0c58ea8d1ab3fa29115d48625c267efe4604d
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-02-14 19:55:42 +0300

    feat: создал метод поиска Job по JobStatus

--- src/main/java/ru/yarigo/mediaconversionservice/conversion/job/model/JobRepository.java
+++ src/main/java/ru/yarigo/mediaconversionservice/conversion/job/model/JobRepository.java
@@ -3,8 +3,10 @@ package ru.yarigo.mediaconversionservice.conversion.job.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+    List<JobEntity> findByStatus(JobStatus jobStatus);
 }
