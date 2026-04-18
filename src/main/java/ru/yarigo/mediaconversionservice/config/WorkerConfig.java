4eb0c66fbecb4210046a1ead2306fdcf98d256ec
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-04-18 16:37:19 +0300

    refactor: изменил конфигурацию thread pool

--- src/main/java/ru/yarigo/mediaconversionservice/config/WorkerConfig.java
+++ src/main/java/ru/yarigo/mediaconversionservice/config/WorkerConfig.java
@@ -14,9 +14,9 @@ public class WorkerConfig {
     @Bean("conversionExecutor")
     public ThreadPoolTaskExecutor conversionExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);
-        executor.setMaxPoolSize(8);
-        executor.setQueueCapacity(100);
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(10);
         executor.setKeepAliveSeconds(60);
         executor.setThreadNamePrefix("conversion-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
